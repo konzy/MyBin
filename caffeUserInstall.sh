c14ed27b76e2689b27038f63ceb412fec00579dc
konzy <bkonzman@gmail.com>
2015-12-13 17:03:59 -0500

    added anaconda to install scripts

--- caffeUserInstall.sh
+++ caffeUserInstall.sh
@@ -1,16 +1,20 @@
 #/bin/bash
 # scripts all the install of caffe 2.0
+cd ~
+echo "download and install anaconda"
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.1-Linux-x86_64.sh
+bash Anaconda2-2.4.1-Linux-x86_64.sh -b
 echo "make Programming directory"
 mkdir ~/Programming/
 cd ~/Programming/
 echo "clone caffe"
 git clone https://github.com/BVLC/caffe.git
-cd caffe
+cd ~
+echo 'export CAFFE_ROOT=~/Programming/caffe' >> .bashrc
+source .bashrc
+cd $CAFFE_ROOT
 mkdir build
 cd build
 cmake ..
 make all
 make runtest
-cd ~
-echo 'export CAFFE_ROOT=~/Programming/caffe' >> .bashrc
-source .bashrc
