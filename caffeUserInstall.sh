749d6e7c11e93a05d1a5fb8058297e8e2db311d0
konzy <bkonzman@gmail.com>
2015-12-12 21:15:12 -0500

    got rid of python install stuff

--- caffeUserInstall.sh
+++ caffeUserInstall.sh
@@ -6,8 +6,6 @@ cd ~/Programming/
 echo "clone caffe"
 git clone https://github.com/BVLC/caffe.git
 cd caffe
-cat python/requirements.txt | xargs -L 1 sudo pip install
-cd ~/Programming/caffe
 mkdir build
 cd build
 cmake ..
